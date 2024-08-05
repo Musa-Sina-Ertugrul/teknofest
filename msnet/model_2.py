7dc7aac9ec9c48ccc7c6707f4f48791af33f3b3c
Musa-Sina-Ertugrul <yutubems@gmail.com>
2024-08-05 10:13:35 +0300

    All done

--- msnet/model_2.py
+++ msnet/model_2.py
@@ -130,11 +130,12 @@ class MFP(torch.nn.Module):
         self.selu_linear = torch.nn.Linear(768,512)
         self.elu_linear = torch.nn.Linear(768, 512)
         self.gelu_linear = torch.nn.Linear(768,512)
-        self.kan = KAN([2*512,512,2*512],spline_order=6,grid_size=100)
+        self.kan = KAN([4*512,2*512,4*512],spline_order=4,grid_size=10)
         # -----------------------------------------------
-        self.linear_1 = torch.nn.Linear(8*512,4*512)
-        self.linear_2 = torch.nn.Linear(4*512,2*512)
-        self.linear_final_1 = torch.nn.Linear(2*512,4)
+        self.linear_1 = torch.nn.Linear(8*512,6*512)
+        self.linear_2 = torch.nn.Linear(6*512,4*512)
+        self.linear_final_1 = torch.nn.Linear(4*512,1280)
+        self.linear_final_2 = torch.nn.Linear(1280,4)
         # -----------------------------------------------
         self.layer_norm_start = torch.nn.LayerNorm(768,eps=1e-8)
         self.layer_norm_final = torch.nn.LayerNorm(8*512,eps=1e-8)
@@ -180,6 +181,8 @@ class MFP(torch.nn.Module):
         x_final = self.elu(x_final)
         x_final = self.kan(x_final)
         x_final = self.linear_final_1(x_final)
+        x_final = self.elu(x_final)
+        x_final = self.linear_final_2(x_final)
         x_final = self.gelu(x_final)
         x_final = self.softmax(x_final)
         return x_final
@@ -210,10 +213,10 @@ class Model(torch.nn.Module):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.mfp = MFP()
-        self.model = AutoModelForCausalLM.from_pretrained("dbmdz/bert-base-turkish-cased")
+        self.model = AutoModelForCausalLM.from_pretrained("FacebookAI/xlm-roberta-base")
         self.model.requires_grad_(True)
-        self.model.cls.predictions.decoder = torch.nn.Flatten(start_dim=2)
-        self.model.cls.predictions.decoder.requires_grad_(True)
+        self.model.lm_head.decoder = torch.nn.Flatten(start_dim=2)
+        self.model.lm_head.decoder.requires_grad_(True)
 
     def forward(self,input_ids: torch.Tensor):
         input_ids = input_ids.to("cuda").long()
